878596e7408729a3a1afae8cefdfe4d753e6eb20
Matthew J. Lewis <lightscalar@gmail.com>
2018-10-02 12:29:54 -0400

    Initial commit of ARGOS pipeline code.

---
+++ one_vs_all_tiles.py
@@ -0,0 +1,211 @@
+"""Build tiles for a one vs many CNN classifier."""
+from vessel import Vessel
+from squares import *
+
+from ipdb import set_trace as debug
+from skimage.io import imread
+from tqdm import tqdm
+
+
+try:
+    maps = Vessel("data/maps.dat")
+    id_to_plant = maps.id_to_plant
+    plant_to_id = maps.plant_to_id
+except:
+    pass
+
+
+def make_image_name(image_object):
+    """Make a reasonable image name to hold all the annotations for that image."""
+    date = image_object["date"]
+    flight = image_object["flight_name"].replace(" ", "_").replace("'", "")
+    image_file = image_object["image_loc"].split("/")[-1].replace(" ", "_")
+    return f"{date}-{flight}-{image_file}.dat"
+
+
+def create_label_maps(path_to_annotations, path_to_maps):
+    """Find species to integer (and inverse) maps."""
+    # Find all unique species.
+    v = Vessel(path_to_annotations)
+    unique_species = set({})
+    for img in v.annotated_images:
+        for annotation in img["annotations"]:
+            if "plant" not in annotation.keys():
+                continue
+            unique_species.add(annotation["plant"])
+    unique_species = sorted(list(unique_species))
+
+    # Build label maps.
+    label_map = {}
+    label_map_inverse = {}
+    for itr, plant_name in enumerate(unique_species):
+        label_map_inverse[plant_name] = itr
+        label_map[itr] = plant_name
+
+    maps = Vessel(path_to_maps)
+    maps.plant_to_id = label_map_inverse
+    maps.id_to_plant = label_map
+    maps.save()
+
+
+def find_target_images(target_species, images):
+    """Find the target species in the annotated image collection."""
+    maps = Vessel("data/label_maps.dat")
+    label_map_inverse = maps.label_map_inverse
+    target_images = []
+    for img in tqdm(images):
+        codes = [
+            label_map_inverse[a["plant"]]
+            for a in img["annotations"]
+            if "plant" in a.keys()
+        ]
+        if target_species in codes:
+            target_images.append(img)
+    return target_images
+
+
+def extract_tiles_from_image(
+    image,
+    annotations,
+    tile_size=128,
+    target_species=None,
+    include_target=True,
+    max_nb_tiles=2000,
+):
+    """Pull the given annotation from the image."""
+    labels = []
+    tiles = np.array([])
+    for annotation in annotations:
+        if "plant" not in annotation.keys():
+            continue
+        if include_target:
+            if plant_to_id[annotation["plant"]] != target_species:
+                continue
+        else:  # require this species
+            if plant_to_id[annotation["plant"]] == target_species:
+                continue
+
+        # Extract tiles, if we're still here.
+        if target_species < 0:
+            debug()
+        col, row = annotation["col"], annotation["row"]
+        col_, row_ = (
+            col * 4000 / annotation["imageWidth"],
+            row * 3000 / annotation["imageHeight"],
+        )
+        tiles_ = np.array(create_rotated_pics(image, row_, col_, tile_size, 25))
+        if tiles.shape[0] > 0:
+            tiles = np.vstack((tiles, tiles_))
+        else:
+            tiles = tiles_
+        # Only return max numbers of tiles.
+        if tiles.shape[0] > max_nb_tiles:
+            break
+    return tiles
+
+
+def add_to_array(X, X_to_add, max_size):
+    """Append array to existing array OR create if necessary."""
+    if X_to_add.shape[0] == 0:
+        return X
+    if X.shape[0] > 0:
+        X = np.vstack((X, X_to_add))
+    else:
+        X = X_to_add
+    if X.shape[0] >= max_size:
+        X = X[:max_size, :]
+    return X
+
+
+def extract_balanced_tiles(
+    path_to_annotations, target_species, tile_size=128, tiles_per_class=2000
+):
+    """Extract equal numbers of target species tiles and tiles from randomly selected species."""
+    maps = Vessel("data/label_maps.dat")
+    data = Vessel(path_to_annotations)
+    images = data.annotated_images
+    nb_images = len(images)
+    target_images = find_target_images(target_species, images)
+    nb_target_images = len(target_images)
+    y_target = np.ones(tiles_per_class)
+    y_other = np.zeros(tiles_per_class)
+    X_target = np.array([])
+    X_other = np.array([])
+    # Grab data from the target set.
+    task_complete = False
+    while not task_complete:
+        image_number = np.random.choice(nb_target_images)
+        image = imread(target_images[image_number]["local_location"])[0]
+        annotations = target_images[image_number]["annotations"]
+        tiles = extract_tiles_from_image(
+            image,
+            annotations,
+            tile_size=tile_size,
+            target_species=target_species,
+            max_nb_tiles=tiles_per_class,
+        )
+        X_target = add_to_array(X_target, tiles, tiles_per_class)
+        task_complete = X_target.shape[0] == tiles_per_class
+
+    # Grab data from random "other" species.
+    task_complete = False
+    while not task_complete:
+        image_number = np.random.choice(nb_images)
+        image = imread(images[image_number]["local_location"])[0]
+        annotations = images[image_number]["annotations"]
+        tiles = extract_tiles_from_image(
+            image,
+            annotations,
+            tile_size=tile_size,
+            target_species=target_species,
+            include_target=False,
+            max_nb_tiles=tiles_per_class,
+        )
+        X_other = add_to_array(X_other, tiles, tiles_per_class)
+        task_complete = X_other.shape[0] == tiles_per_class
+
+    idx = np.arange(tiles_per_class * 2)
+    np.random.shuffle(idx)
+    X = np.vstack((X_target, X_other))
+    y = np.hstack((y_target, y_other))
+    return X[idx, :], y[idx]
+
+
+if __name__ == "__main__":
+
+    for itr in range(10):
+        X, y = extract_balanced_tiles("data/annotated_images.dat", 28)
+
+    # create_maps = False
+    # if create_maps:
+    #     create_label_maps("data/annotated_images.dat")
+    # else:
+    #     maps = Vessel("data/label_maps.dat")
+
+    # # Now extract tiles on an image by image basis.
+    # tile_size = 128
+    # for img_obj in v.annotated_images:
+    #     filename = make_image_name(img_obj)
+    #     path_to_tile_file = f"data/tiles/{filename}"
+    #     td = Vessel(path_to_tile_file)
+    #     td.tiles = np.array([])
+    #     td.labels = []
+    #     img = imread(img_obj["local_location"])[0]
+    #     for annotation in tqdm(img_obj["annotations"]):
+    #         if "plant" not in annotation.keys():
+    #             continue
+    #         plant_name = annotation["plant"]
+    #         col, row = annotation["col"], annotation["row"]
+    #         col_, row_ = (
+    #             col * 4000 / annotation["imageWidth"],
+    #             row * 3000 / annotation["imageHeight"],
+    #         )
+    #         tiles = np.array(create_rotated_pics(img, row_, col_, tile_size, 10))
+    #         if td.tiles.shape[0] > 0:
+    #             td.tiles = np.vstack((td.tiles, tiles))
+    #         else:
+    #             td.tiles = tiles
+    #         for k in range(tiles.shape[0]):
+    #             td.labels.append(label_map_inverse[plant_name])
+    #     debug()
+    #     td.save()
---
+++ squares.py
@@ -0,0 +1,72 @@
+import numpy as np
+import cv2
+import os
+import imutils
+from ipdb import set_trace as debug
+
+
+def create_rotated_pics(img, row, col, size, num_rotations):
+    """Extract rotated tiles from an image."""
+
+    images = []
+
+    rows, cols, chans = img.shape
+
+    shift_right = col < size / 2
+    shift_left = col > cols - size / 2
+    shift_down = row < size / 2
+    shift_up = row > rows - size / 2
+
+    #    os.makedirs('New_images')
+    if shift_right + shift_left + shift_down + shift_up > 0:
+        if shift_right:
+            col = size / 2
+        if shift_left:
+            col = cols - size / 2
+        if shift_down:
+            row = size / 2
+        if shift_up:
+            row = rows - size / 2
+        col_low = int(col - size / 2)
+        col_high = int(col + size / 2)
+        row_low = int(row - size / 2)
+        row_high = int(row + size / 2)
+        square = img[row_low:row_high, col_low:col_high, :]
+        images.append(square)
+
+    elif (
+        col < size * np.sqrt(2) / 2
+        or col > cols - size * np.sqrt(2) / 2
+        or row < size * np.sqrt(2) / 2
+        or row > rows - size * np.sqrt(2) / 2
+    ):
+        col_low = int(col - size / 2)
+        col_high = int(col + size / 2)
+        row_low = int(row - size / 2)
+        row_high = int(row + size / 2)
+        square = img[row_low:row_high, col_low:col_high, :]
+        images.append(square)
+    #        cv2.imwrite('New_images/image.jpg',square)
+
+    else:
+        col_low = int(col - np.sqrt(2) * size / 2)
+        col_high = int(col + np.sqrt(2) * size / 2)
+        row_low = int(row - np.sqrt(2) * size / 2)
+        row_high = int(row + np.sqrt(2) * size / 2)
+        angle_per_iteration = int(360 / num_rotations)
+        img_ = img[row_low:row_high, col_low:col_high, :]
+        ctr = int(np.sqrt(2) * size / 2)
+        ctr_plus = int(ctr + size / 2)
+        ctr_minus = int(ctr - size / 2)
+
+        for itr in range(num_rotations):
+            rotation_angle = angle_per_iteration * itr
+            rotated_image = imutils.rotate(img_, rotation_angle)
+            square = rotated_image[ctr_minus:ctr_plus, ctr_minus:ctr_plus, :]
+            images.append(square)
+
+    return images
+
+
+if __name__ == "__main__":
+    create_rotated_pics(1200, 0, 200, 4)
